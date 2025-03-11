a6a25fa35a119ad3b2fd6bf1457995f6c99ac384
Julia Lacatusu <jlacatusu@gmail.com>
2025-03-11 15:38:55 +0200

    lab 3 problem 1

---
+++ LAB3/main.cpp
@@ -0,0 +1,21 @@
+#include "Math.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Add(2, 3) = " << Math::Add(2, 3) << endl;
+    cout << "Add(2, 3, 4) = " << Math::Add(2, 3, 4) << endl;
+    cout << "Add(2.5, 3.5) = " << Math::Add(2.5, 3.5) << endl;
+    cout << "Add(2.5, 3.5, 4.5) = " << Math::Add(2.5, 3.5, 4.5) << endl;
+
+    cout << "Mul(2, 3) = " << Math::Mul(2, 3) << endl;
+    cout << "Mul(2, 3, 4) = " << Math::Mul(2, 3, 4) << endl;
+    cout << "Mul(2.5, 3.5) = " << Math::Mul(2.5, 3.5) << endl;
+    cout << "Mul(2.5, 3.5, 4.5) = " << Math::Mul(2.5, 3.5, 4.5) << endl;
+
+    cout << "Add(5, 1, 2, 3, 4, 5) = " << Math::Add(5, 1, 2, 3, 4, 5) << endl;
+    cout << "Add of both string : " << Math::Add("Buna", "Zuia") << endl;
+
+    return 0;
+}
